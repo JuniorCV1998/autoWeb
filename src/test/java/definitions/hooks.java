10cb0eba5ded0456856e3fdb0ac8a4cdca1b68c7
JuniorCV1998 <you@example.com>
2023-06-01 20:00:33 -0500

    creando proyecto

---
+++ src/test/java/definitions/hooks.java
@@ -0,0 +1,14 @@
+package definitions;
+
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+
+public class hooks {
+    public static WebDriver driver;
+
+    @Before
+    public static void setup(){
+
+    }
+
+}
