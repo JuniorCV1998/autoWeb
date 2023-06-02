f2e663d2ab6f81926822e44e4455f4390feb4edf
JuniorCV1998 <you@example.com>
2023-06-01 20:59:09 -0500

    clase hooks

--- src/test/java/definitions/hooks.java
+++ src/test/java/definitions/hooks.java
@@ -1,14 +1,26 @@
 package definitions;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class hooks {
+public class hooks { // se configura el antes y despues del test
     public static WebDriver driver;
 
-    @Before
+    @Before //Configuración
     public static void setup(){
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+        //src/test/resources/drivers/chromedriver.exe
+        driver = new ChromeDriver(); // abre navegador
+        driver.manage().window().maximize(); //maximizamos ventana
 
+    }
+
+    @After //Demoles
+    public static void tearDown(){
+        driver.manage().deleteAllCookies();
+        driver.close();
     }
 
 }
